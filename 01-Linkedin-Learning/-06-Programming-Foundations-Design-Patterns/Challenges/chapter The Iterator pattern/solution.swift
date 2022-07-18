d07173c0de4758e5494d2f7279f6e7ad8628cee2
ziad-shaaban <96917786+ziad-shaaban@users.noreply.github.com>
2022-07-18 16:49:05 +0200

    Delete 01-Linkedin-Learning/-06-Programming-Foundations-Design-Patterns/Challenges/chapter The Iterator pattern directory

--- 01-Linkedin-Learning/-06-Programming-Foundations-Design-Patterns/Challenges/chapter The Iterator pattern/solution.swift
+++
@@ -1,9 +0,0 @@
-let animals = ["Great Auk", "Dodo", "Stellar's Sea Cow", "Hawaiian Tree Snail"]
-for animal in animals{
-    print(animal)
-}
-
-var animalIterator = animals.makeIterator()
-while let animal = animalIterator.next() {
-    print(animal)
-}
